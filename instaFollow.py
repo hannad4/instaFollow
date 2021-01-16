7522c1bda461c9a26ba0597917e83c3b151155cd
Danny Hanna <hannad4@tcnj.edu>
2021-01-16 10:53:43 -0500

    fixed follower list yield exception

--- instaFollow.py
+++ instaFollow.py
@@ -21,23 +21,26 @@ def obtainFollowers(browser):
         for follower_index in range(group, group + 12):
             yield waiter.find_element(browser, follower_css.format(follower_index)).text
 
-        # Keep last element from going stale    
+        # Keep last element from going stale (aka make sure the scrolling view doesn't forget who the last follwer was)
         last_follower = waiter.find_element(browser, follower_css.format(follower_index))   
         browser.execute_script("arguments[0].scrollIntoView();", last_follower)
 
 def navigateToFollowers(browser): 
     print("\nGathering your followers\n")
-    followerHREF = "/" + userName + "/followers/"
+    followerHREF = "/" + userName + "/followers/" 
+    totalFollowerCount = WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.presence_of_element_located((By.XPATH, '//li/a[text()=" followers"]/span'))).text
     WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.element_to_be_clickable((By.XPATH, '//a[@href="'+followerHREF+'"]'))).click() 
     global followerList; 
     followerList = []; 
     for count, follower in enumerate(obtainFollowers(browser)):
-        # print("{}: {}".format(count, follower))
+        print("{}: {}".format(count, follower))
         followerList.append(follower)
-    print("The follower list is: " + followerList)
+        if count >= int(totalFollowerCount) - 1:
+            break; 
+    # print("\nYour total follower list is:\n\n" + ' ||| '.join(followerList) + "\n")
 
 
-def navigateProfile(browser): # User has definitely logged in. These elements undeniably exist, so no error handling is needed
+def navigateProfile(browser): # User has definitely logged in at this point. These elements exist for all users, so no error handling is needed
     print("Navigating to your profile...\n")
     WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/nav/div[2]/div/div/div[3]/div/div[5]/span"))).click()
     WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div[2]/div[2]/a[1]"))).click()
@@ -60,28 +63,29 @@ def attemptLogin(userName, passWord, browser):
                 os.system('clear')
             print("\nThe login info you entered was incorrect\n")
             main()
-        except:  # Assuming no slfErrorAlert loaded means login credentials accepted
+        except:     # Assuming no slfErrorAlert loaded means login credentials accepted
             print("\nLogged in successfully\n")
             navigateProfile(browser)
 
-    except Exception as e:
+    except Exception as e:  # If something went wrong, this exception block will catch it & land the program gently instead of a messy crash
         print("\nSomething went wrong. The program has been halted\n")
-        print("\nYou can provide the following error message to the developer:\n")
+        print("\nYou can provide the following error message (if available) to the developer:\n")
         print("\n" + str(e) + "\n")         
         return
 
 
 def main(): 
-    global userName; 
+    global userName;            # We will use the userName in many functions
 
     userName = input("\nEnter your instagram username. This is case sensitive:\n")
     passWord = getpass.getpass("\nEnter your password. This will be used to log into your Instagram and will not be shared anywhere else. For privacy, typing your password will not move the cursor or show characters:\n")
 
-    # Enabling experimental options to allow for a persistent browser to exist for the user even if program terminated
+    # Enabling experimental options to allow for a persistent browser to exist (but minimized) for the user even if program terminated
     chrome_options = Options() 
     chrome_options.add_experimental_option("detach", True)
     browser = webdriver.Chrome(options=chrome_options, executable_path=ChromeDriverManager().install())
-
+    print("\nMinimizing window\n")
+    
     browser.get("https://www.instagram.com/")
     attemptLogin(userName, passWord, browser)
 
