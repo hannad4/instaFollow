772edf31f8cb1dcbce560ec40376b3129a29dbff
Danny Hanna <hannad4@tcnj.edu>
2021-01-20 17:24:18 -0500

    formatting termnial output

--- instaFollow.py
+++ instaFollow.py
@@ -5,11 +5,8 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException        
+from selenium.webdriver.support import expected_conditions as EC   
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from explicit import waiter
 
 import os    # Utilized only for knowing which command to use for clearing terminal (easy viewing for noobs)
 import getpass  # Utilized only for obtaining the users password without showing it out in the open
@@ -17,30 +14,36 @@ import itertools # Utilized only for iteration through scraping process
 import gc       # Utilied only for memory cleanup at the end of the program for a more clean termination
 
 def cleanClose():
-    print("\nSorry if you got dissappointing results.\n")
-    print("\nThanks for using instaFollow.\n")
-    gc.collect()
-    quit()
+    greenPrint("\nThanks for using instaFollow.\n")
+    if input('\33[32m' + "Press [Enter] to exit\n" + '\033[0m'):
+        gc.collect()
+        quit()
 
 def listCompare():
-    print("\n\nComparing Lists now\n")
+    greenPrint("\n\nComparing Lists now\n")
     notFollowing = list(set(followerList) - set(followingList))  # These people follow you, but you do not follow them back 
     notFollower = list(set(followingList) - set(followerList))  # You are following these people, but they are not following you back
-    print("\nThis is the final list of people who follow you, but you do not follow them back:\n" + ' || '.join(notFollowing) + "\n")
-    print("\nThis is the final list of people that you are following, but they do not follow you back:\n" + ' || '.join(notFollower) + "\n")
-    writeToFile = input("\nDo you want to write these results to a text file? Y/N\n")
+    match = list(set(followerList) & set(followingList)) # These people are on both lists (the good peeps)
+    greenPrint("\nThis is the final list of people who follow you, but you do not follow them back:\n\n" + ' || '.join(notFollowing) + "\n")
+    greenPrint("\nThis is the final list of people that you are following, but they do not follow you back:\n\n" + ' || '.join(notFollower) + "\n")
+    greenPrint("\nThis is the final list of people who follow you, & you follow them back:\n\n" + ' || '.join(match) + "\n")
+    writeToFile = input('\33[45m' + "\nDo you want to write these results to a text file? Y/N\n" + '\033[0m')
     if(writeToFile.upper() == "YES" or writeToFile.upper() == "Y"):
-        print("\nThe file will be located in the same location as this program. It is titled 'instaResults.txt'\n")
+        greenPrint("\nThe file will be located in the same location as this program. It is titled 'instaResults.txt'\n")
         instaResults = open("instaResults.txt", "w")
+
         instaResults.write("List 1 - People who follow you, but you do not follow them back:\n\n")
-
         for count, following in enumerate(notFollowing):
-            instaResults.write(("{}: {}\n".format(count, following)))
+            instaResults.write(("{:02d}: {}\n".format(count+1, following)))
         
-        instaResults.write("\n\n=================================\n\nList 2 - People that you are following, but they do not follow you back:\n\n")
-
+        instaResults.write("\n=================================\n\nList 2 - People that you are following, but they do not follow you back:\n\n")
         for count, follower in enumerate(notFollower):
-            instaResults.write(("{}: {}\n".format(count, follower)))
+            instaResults.write(("{:02d}: {}\n".format(count+1, follower)))
+
+        instaResults.write("\n=================================\n\nList 3 - These people follow you, and you follow them:\n\n")
+        for count, matchingUser in enumerate(match):
+            instaResults.write(("{:02d}: {}\n".format(count+1, matchingUser)))
+
         instaResults.close()
         cleanClose()
 
@@ -48,14 +51,14 @@ def listCompare():
         cleanClose()
 
 def navigateToFollowing(browser):
-    print("\nGathering your following list", end = "")
+    print('\33[32m' + "\nGathering your following list" + '\033[0m', end = "")
     followingHREF = "/" + userName + "/following/"
     totalFollowingCount = WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.presence_of_element_located((By.XPATH, '//li/a[text()=" following"]/span'))).text
     WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.element_to_be_clickable((By.XPATH, '//a[@href="'+followingHREF+'"]'))).click() 
     global followingList 
     followingList = [] 
     for count, following in enumerate(obtainList(browser)):
-        print(".", end = "")
+        print('\33[32m' + "." + '\033[0m', sep=' ', end='', flush=True)
         followingList.append(following)
         if count >= int(totalFollowingCount) - 1:
             break 
@@ -65,23 +68,25 @@ def navigateToFollowing(browser):
 
 def obtainList(browser): 
     follower_css = "ul div li:nth-child({}) a.notranslate"  # CSS's nth-child functionality lets you pick all follower children
+    global follower_index   # have to make this global to avoid warning about possibleUnboundVariable when assigning last_follower
     for group in itertools.count(start=1, step=12):     # Step through 12 at a time to scroll follower list
         for follower_index in range(group, group + 12):
-            yield waiter.find_element(browser, follower_css.format(follower_index)).text
+            yield WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.presence_of_element_located((By.CSS_SELECTOR, follower_css.format(follower_index)))).text
+            # yield waiter.find_element(browser, follower_css.format(follower_index)).text
 
         # Keep last element from going stale (aka make sure the scrolling view doesn't forget who the last follwer was)
-        last_follower = waiter.find_element(browser, follower_css.format(follower_index))   
+        last_follower = WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.presence_of_element_located((By.CSS_SELECTOR, follower_css.format(follower_index))))     
         browser.execute_script("arguments[0].scrollIntoView()", last_follower)
 
 def navigateToFollowers(browser): 
-    print("\nGathering your followers list", end = "")
+    print('\33[32m' + "\nGathering your followers list" + '\033[0m', end = "")
     followerHREF = "/" + userName + "/followers/" 
     totalFollowerCount = WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.presence_of_element_located((By.XPATH, '//li/a[text()=" followers"]/span'))).text
     WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.element_to_be_clickable((By.XPATH, '//a[@href="'+followerHREF+'"]'))).click() 
     global followerList 
     followerList = [] 
     for count, follower in enumerate(obtainList(browser)):
-        print(".", end = "")
+        print('\33[32m' + "." + '\033[0m', sep=' ', end='', flush=True)
         followerList.append(follower)
         if count >= int(totalFollowerCount) - 1:
             break 
@@ -90,7 +95,11 @@ def navigateToFollowers(browser):
 
 
 def navigateProfile(browser): # User has definitely logged in at this point. These elements exist for all users, so no error handling is needed
-    print("\nNavigating to your profile...\n")
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+    greenPrint("\nNavigating to your profile...\n")
     WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/nav/div[2]/div/div/div[3]/div/div[5]/span"))).click()
     WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div[2]/div[2]/a[1]"))).click()
     navigateToFollowers(browser) 
@@ -98,8 +107,7 @@ def navigateProfile(browser): # User has definitely logged in at this point. The
 
 def attemptLogin(userName, passWord, browser): 
     try:  # Many things can go wrong in login attempt, hence try except block (in case the world explodes?!?!)
-        print("An automated browser will be opened. You may keep it in the background but DO NOT MINIMZE IT.\nPlease do not interfere with it while the program is running\n")
-        print('\nEntering credentials...\n')
+        greenPrint('\nEntering credentials...\n')
         WebDriverWait(browser, WAIT_TIME_GLOBAL).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='username']"))).send_keys(userName)
         browser.find_element_by_xpath("//input[@name='password']").send_keys(passWord)
         browser.find_element_by_xpath("//button/div[text()='Log In']").click()
@@ -112,31 +120,38 @@ def attemptLogin(userName, passWord, browser):
                 os.system('cls')
             else:
                 os.system('clear')
-            print("\nThe login info you entered was incorrect\n")
+            greenPrint("\nThe login info you entered was incorrect\n")
             main()
         except:     # Assuming no slfErrorAlert loaded means login credentials accepted
-            print("\nLogged in successfully\n")
+            greenPrint("\nLogged in successfully\n")
             navigateProfile(browser)
 
     except Exception as e:  # If something went wrong, this exception block will catch it & land the program gently instead of a messy crash
-        print("\nSomething went wrong. The program has been halted\n")
-        print("\nYou can provide the following error message (if available) to the developer:\n")
-        print("\n" + str(e) + "\n")         
+        greenPrint("\nSomething went wrong. The program has been halted\n")
+        greenPrint("\nYou can provide the following error message (if available) to the developer:\n")
+        greenPrint("\n" + str(e) + "\n")         
         return
+
+def greenPrint(message):
+    CGREEN = '\33[32m'
+    CEND = '\033[0m'
+    print(CGREEN + message + CEND)
+    return
 
 
 def main(): 
     global userName            # We will use the userName in many functions
-    print("\nWelcome to instaFollow. This program will automate the process of comparing your followers & following list on Instagram\n")
-    print("\nThis program will not share any data collected. The source code is available at https://github.com/hannad4/instaFollow\n\n")    
+    os.system("")              # Hack to make Windows command prompt show color text output
+    
+    greenPrint("\nWelcome to instaFollow. This program will automate the process of comparing your followers & following list on Instagram\n")
+    greenPrint("\nAll data collected by this program is flushed upon termination, and will not be shared with anyone. The source code is available at https://github.com/hannad4/instaFollow\n")    
 
-    userName = input("\nEnter your instagram username. This is case sensitive:\n")
-    passWord = getpass.getpass("\nEnter your password. This will be used to log into your Instagram and will not be shared anywhere else. For privacy, typing your password will not move the cursor or show characters:\n")
+    userName = input('\33[45m' + "Enter your instagram username. This is case sensitive:\n" + '\033[0m')
+    passWord = getpass.getpass('\33[45m' + "\nEnter your password. This will be used to log into your Instagram and will not be shared anywhere else. For privacy, typing your password will not move the cursor or show characters:\n" + '\033[0m')
 
-    # Enabling experimental options to allow for a persistent browser to exist (but minimized) for the user even if program terminated
-    chrome_options = Options() 
-    chrome_options.add_experimental_option("detach", True)
-    browser = webdriver.Chrome(options=chrome_options, executable_path=ChromeDriverManager().install())    
+    greenPrint("\nAn automated browser will be opened. You may keep it in the background but DO NOT MINIMZE IT.\n\nPlease do not interfere with it while the program is running\n")
+
+    browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())    
     browser.get("https://www.instagram.com/")
     attemptLogin(userName, passWord, browser)
 
